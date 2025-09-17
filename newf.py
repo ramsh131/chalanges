53db8e09ec31b64ccb3f8c6322f4ba9f347fbc67
ramsh131 <ramshur131@gmail.com>
2025-09-17 12:59:25 +0530

    Add files via upload

---
+++ newf.py
@@ -0,0 +1,34 @@
+a = 5
+b = 5
+print(a == b)
+a = 4
+b = 7
+print(a != b)
+a = 6
+b = 4
+print(a != b)
+a = 5
+b = 3
+print(a > b)
+a = 9
+b = 4
+print(a > b)
+a = 2
+b = 5
+print(a > b)
+a = 3
+b = 8
+print(a < b)
+a = 12
+b = 4
+print(a < b)
+a = 5
+b = 8
+if a > b :
+    print(a is largest )
+        
+else:
+     print(b is largest)
+ num = 56
+ if num % 2 == 0:
+    print()   
